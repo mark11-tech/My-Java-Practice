317a6814c31003fbe287e5fcbf8005fba8ff5cb5
mark11-tech <gaymarkvincent@gmail.com>
2025-09-25 09:44:25 +0800

    update

--- Hello World/Hello.java
+++ Hello World/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        
-    System.out.println("Hello, World!");
+    
+    String name = "Mark";
+
+    System.out.println("Hello, " + name);
 
     }
 }
